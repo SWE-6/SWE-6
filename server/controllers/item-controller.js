3ee5987dbe6a4beaa572da015a0fc8468d067398
Lambientan <LambientanYT@gmail.com>
2022-02-15 01:26:26 -0500

    Finished the controller functions for CRUD opperation with exams collection and modified patter for REST API. API is fully operational.

--- server/controllers/item-controller.js
+++ server/controllers/item-controller.js
@@ -1,9 +1,18 @@
+/**
+* Pedro Gutierrez Rincon
+* 2022-02-15
+* the controllers for CRUD opperations are defined here.
+* Called by router.
+**/
+
 /* eslint-disable no-undef, arrow-body-style */
 //const Item = require('../models/item-model'); //from the template, deprecated
 const patients = require("../models/patient-model.js");
 const exams = require("../models/exam-model.js");
 //NOTE:requiring express-async-handeler for eassy error handeling of async functions
 const asyncHandler = require("express-async-handler");
+
+//CONTROLLERS for patients
 
 /**@desc gets all the patients from the database
 */
@@ -19,6 +28,8 @@ getPatientByID = asyncHandler( async (req, res) => {
   res.status(200).json(patient);
 });
 
+/**@desc creates a new patient
+*/
 createPatient = asyncHandler( async (req, res) => {
   const body = req.body;
   if (!body) //exception hadeling for body
@@ -31,6 +42,8 @@ createPatient = asyncHandler( async (req, res) => {
   res.status(200).json(newPatient);
 });
 
+/**@desc updates a specific patient
+*/
 updatePatient = asyncHandler( async (req, res) => {
   const patient = await patients.findById(req.params["id"]); //finding the patient:
   if (!patient) //checking if patient was obtained
@@ -43,6 +56,8 @@ updatePatient = asyncHandler( async (req, res) => {
   res.status(200).json(updatedPatient);
 });
 
+/**@desc deletes a specific patient
+*/
 deletePatient = asyncHandler( async (req, res) => {
   //find patient before attempting to delete
   const patient = await patients.findById(req.params["id"]);
@@ -55,42 +70,46 @@ deletePatient = asyncHandler( async (req, res) => {
   await patients.findOneAndDelete({_id: req.params.id}); //Did work
 
   res.status(200).json({_id: req.params["id"]}); //just return the id back
-  //res.status(200).json(patient);
 });
 
-//CONTROLLERS for the exams:
+//CONTROLLERS for exams:
 
 /**@desc gets all the exams
 */
-//FIXME: returns null for some reason
 getExams = asyncHandler( async (req, res) => {
   const allExams = await exams.find({}); //passed empty object returns all member of the set
-  res.status(200).json(allExams); //FIXME: change name to allExams
+  res.status(200).json(allExams);
 });
 
 //
 /**@desc gets exams of the corresponding patians by ID.
 */
-//FIXME: returns null for some reason
-getExamsByID = asyncHandler( async (req, res) => //FIXME: change name to getExamsByID
+getExamsOfPatient = asyncHandler( async (req, res) =>
 {
-  const examOfPatient = await exams.find({patientID: req.params["id"]});
+  const examsOfPatient = await exams.find({patientId: req.params.patientId});
 
-  if (!examOfPatient) //error handeling
+  if (!examsOfPatient) //error handeling
   {
     res.status(400);
     throw new Error("Exam not found"); //throws error and breaks away from function
   }
 
-  res.status(200).json(examOfPatient);
+  res.status(200).json(examsOfPatient);
 });
 
 /**@desc gets an exam from a patient using a provided ID and index.
 */
-//FIXME: is just a template as of now
-getExamByIndex = asyncHandler( async (req, res) =>
+getExamByID = asyncHandler( async (req, res) =>
 {
-  res.status(400).json( {message: "getExamByIndex called successfully"} );
+  const exam = await exams.find({_id: req.params["id"]});
+
+  if (!exam) //error handeling
+  {
+    res.status(400);
+    throw new Error("Exam not found"); //throws error and breaks away from function
+  }
+
+  res.status(200).json(exam);
 });
 
 /**@desc creates a new exam for a given patient by ID
@@ -110,18 +129,35 @@ createExam = asyncHandler( async (req, res) =>
 
 /**@desc updates an exam from a patient using a provided ID and index
 */
-//FIXME: is just a template as of now
 updateExam = asyncHandler( async (req, res) =>
 {
-  res.status(200).json( {message: "updateExam called successfully"} );
+  console.log(req.params);
+  const exam = await exams.find({ _id: req.params["id"], patientId: req.params["patientId"] }); //finding the exam:
+  if (!exam) //checking if exam was obtained
+  {
+    res.status(400);
+    throw new Error("Patient was not found");
+  }
+  const updatedExam = await exams.findByIdAndUpdate(req.params["id"], req.body, {new: true});
+
+  res.status(200).json(updatedExam);
 });
 
 /**@desc deletes an exam from a patient using a provided ID and index
 */
-//FIXME: is just a template as of now
 deleteExam = asyncHandler( async (req, res) =>
 {
-  res.status(200).json( {message: "deleteExam called successfully"} );
+  //find exam before attempting to delete
+  const exam = await exams.findById(req.params["id"]);
+  if (!exam)
+  {
+    rest.status(400);
+    throw new Error("Patient not found");
+  }
+
+  await exams.findOneAndDelete({_id: req.params.id}); //Did work
+
+  res.status(200).json({_id: req.params["id"]}); //just return the id back
 });
 
 module.exports = {
@@ -131,8 +167,8 @@ module.exports = {
   updatePatient,
   deletePatient,
   getExams,
-  getExamsByID,
-  getExamByIndex,
+  getExamsOfPatient,
+  getExamByID,
   createExam,
   updateExam,
   deleteExam,
